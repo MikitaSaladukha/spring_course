24ee10292f91ba3bf753d7ca2ea246af2edb587b
Mikita Saladukha <malkomus@gmail.com>
2023-10-24 15:22:29 +0300

    hibernate test: SessionFactory good, Employee not good

--- src/main/java/com/mikkita/spring_course/hibernate_test/Test1.java
+++ src/main/java/com/mikkita/spring_course/hibernate_test/Test1.java
@@ -1,8 +1,9 @@
 package com.mikkita.spring_course.hibernate_test;
 
 import com.mikkita.spring_course.hibernate_test.entity.Employee;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-
+import org.hibernate.cfg.Configuration;
 
 public class Test1 {
     public static void main(String[] args) {
@@ -10,15 +11,15 @@ public class Test1 {
                 .configure("hibernate.cfg.xml")
                 .addAnnotatedClass(Employee.class)
                 .buildSessionFactory();
-//        try {
-//            Session session = factory.getCurrentSession();
-//            Employee emp = new Employee("Mikita", "Saladukha", "cyber", 2000);
-//            session.beginTransaction();
-//            session.save(emp);
-//            session.getTransaction().commit();
-//        }
-//        finally {
-//            factory.close();
-//        }
+        try {
+            Session session = factory.getCurrentSession();
+            Employee emp = new Employee("Mikita", "Saladukha", "cyber", 2000);
+            session.beginTransaction();
+            session.save(emp);
+            session.getTransaction().commit();
+        }
+        finally {
+            factory.close();
+        }
     }
 }
